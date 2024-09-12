1ed81a5439eba984847deaf045e92682692605e1
adhikara <wadhikara@gmail.com>
2024-09-12 15:10:00 +0700

    commit3

--- index.php
+++ index.php
@@ -4,17 +4,26 @@ require 'vendor/autoload.php';
 
 use Masehi\Converter as MasehiConverter;
 
+// Inisialisasi Converter
 $masehi = new MasehiConverter;
 
+// Tampilkan tanggal saat ini
 echo MasehiConverter::convertDate(array(
     "date" => "now",
     "format" => "l, d M Y"
-  ));
-  echo "\n";
-  # Output: Minggu, 02 Des 2018
-
-echo terbilang(421);
-
+));
 echo "\n";
 
+?>
+
+<form method="POST">
+    Masukan Angka: <input type="text" name="angka" />
+    <input type="submit" value="Convert" />
+</form>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $angka = $_POST['angka'];
+    echo terbilang(trim($angka));
+}
 ?>
